ff34464c391cc22f4e824e242e33a9e2baaa0c5a
Renato <renatoribroman@gmail.com>
2022-08-02 16:21:29 -0300

    remove pages & add redirects

--- redirects.js
+++ redirects.js
@@ -1,4 +1,13 @@
 const perm = (source, destination) => ({ source, destination, permanent: true });
 // const temp = (source, destination) => ({ source, destination, permanent: false });
 
-module.exports = [perm("/quiz/all", "/quiz/world"), perm("/quiz/:name*", "/flags/:name*")];
+module.exports = [
+  // Fist refac
+  perm("/quiz/all", "/quiz/world"),
+  perm("/quiz/:name*", "/flags/:name*"),
+
+  // Second refac
+  perm("/flags/:name*", "play/flags/:name*"),
+  perm("/shapes/:name*", "play/shapes/:name*"),
+  perm("/others/world-map", "play/map/world"),
+];
